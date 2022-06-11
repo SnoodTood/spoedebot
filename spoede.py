55de859315e5902bae21a580d30d22e8946068b0
Alexander <wu3567@umd.edu>
2022-06-11 17:31:27 -0400

    Added functionality to fetch rhymes from the web using BeautifulSoup

--- spoede.py
+++ spoede.py
@@ -1,4 +1,5 @@
-from urllib import requests
+from urllib import request
+from bs4 import BeautifulSoup
 import json
 import random
 
@@ -31,7 +32,9 @@ def count_syll(word):
 
 # Query rhymer.com to find appropriate rhymes for a word
 # Attempt to rhyme as many syllables as possible by adjusting the URL
+# N.B. Can throw errors if internet issues cause urlopen to fail
 
+last = "last-syllable-rhymes/"
 double = "double-rhymes/"
 triple = "triple-rhymes/"
 
@@ -39,6 +42,8 @@ def fetch_rhymes(word):
     syll = count_syll(word)
     url = "https://www.rhymer.com/"
     
+    if syll == 2:
+        url += last
     if syll == 3:
         url += double
     if syll > 3:
@@ -47,16 +52,33 @@ def fetch_rhymes(word):
     url += ".html"
     
     with request.urlopen(url) as response:
-        html = response.read()
+        soup = BeautifulSoup(response, 'html.parser')
     
-    # [...]
+    # rhymer.com result pages categorize results by syllables inside a <div>
+    # tag with id of e.g. "syl-1" for 1-syllable rhymes
+    target_id = "syl-" + str(syll)
+    
+    # BeautifulSoup can then be used to search for said tag
+    div = soup.find(id = target_id)
+    if div == None:
+        return []
+    
+    out = list()
+    for el in div.children:
+        if el.name == 'a':
+            out.append(el.string)
+    
+    return out
+
 
 # If rhymer.com has no entries or fetch_rhymes() otherwise fails to provide,
 # generate a rhyme by chopping off the first syllable of the word and
 # replacing it with a randomly-generated one
 
 def gen_rhyme(word):
+    return
     # [...]
+
 
 # Put everything together to generate a complete phrase
 
